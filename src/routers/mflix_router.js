38ec5300d962d0b1fc8787278e3b3ca0e30e00d8
Jose Mendoza <joseemendoza194@gmail.com>
2020-01-25 17:07:58 -0800

    Implemented theater model and user model

--- src/routers/mflix_router.js
+++ src/routers/mflix_router.js
@@ -3,8 +3,12 @@ const router = new express.Router()
 
 const movies = require('../models/sample_mflix/movies_model')
 const comments = require('../models/sample_mflix/comment_model')
+const user = require('../models/sample_mflix/user_model')
+const theater = require('../models/sample_mflix/theater_model')
 
 //We need to make more routes for different questions. Possibly at least 10
+
+//Movies collection
 router.get('/questions/1', async (req, res) => {
     //We might need to delete this so users can fill in the blanks
 
@@ -17,11 +21,31 @@ router.get('/questions/1', async (req, res) => {
     
 })
 
+//Comments Collection
 router.get('/questions/2', async (req, res) => {
-    //We might need to delete this so users can fill in the blanks
-
     try{
         const data = await comments.findById('5a9427648b0beebeb6957a08')
+        res.status(200).send(data)
+    }catch(error){
+        res.status(400).send(error)
+    }
+    
+})
+
+//Users collection
+router.get('/questions/3', async (req, res) => {
+    try{
+        const data = await user.findById('59b99db5cfa9a34dcd7885b9')
+        res.status(200).send(data)
+    }catch(error){
+        res.status(400).send(error)
+    }
+    
+})
+
+router.get('/questions/4', async (req, res) => {
+    try{
+        const data = await theater.findById('59a47286cfa9a3a73e51e73b')
         res.status(200).send(data)
     }catch(error){
         res.status(400).send(error)
